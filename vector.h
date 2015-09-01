aa54f19627a71534f66f66aca4e7d2146534ed1d
Hampus Liljekvist <hlilje@users.noreply.github.com>
2015-09-01 16:07:59 +0200

    Initial commit

---
+++ vector.h
@@ -0,0 +1,29 @@
+class UIntVector
+{
+    private:
+        std::size_t size;
+    public:
+        UIntVector();
+
+        UIntVector(UIntVector const&);
+
+        UIntVector(std::size_t);
+
+        UIntVector(std::initializer_list);
+
+        UIntVector(UIntVector);
+
+        ~UIntVector();
+
+        int& operator[](int x);
+
+        unsigned int& operator[](int x);
+
+        const unsigned int& operator[](int x) const;
+
+        UIntVector& operator=(A const& UIntVector);
+
+        void reset();
+
+        std::size_t size();
+};
